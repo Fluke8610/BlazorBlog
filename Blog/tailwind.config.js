8479caf9eecdd2f1e59b99e3465a8342ba81c10c
Chris Moore <cdl.moore86@gmail.com>
2024-11-27 01:13:51 +0000

    Updated to use .Net 9.0
    Updated theme colours and layout.
    
    Re-working content and layout.

--- Blog/tailwind.config.js
+++ Blog/tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
             'xxl': '1920px',
             'xxxl': '2560px'
         },
+        colors: {
+            'space-cadet': '#2b2d42',
+            'cool-gray': '#8d99ae',
+            'anti-flash-white': '#edf2f4',
+            'red-pantone': '#ef233c',
+            'fire-engine-red': '#d90429',
+            'white': '#ffffff',
+            'black' : '#000000',
+            'eggshell-blue': '#bfd7ed',
+            'light-blue': '#60a3d9',
+            'mid-blue': '#0074b7',
+            'dark-blue': '#003b73',
+            'off-white': '#fffbfb'
+        },
         extend: {},
   },
   plugins: [],
